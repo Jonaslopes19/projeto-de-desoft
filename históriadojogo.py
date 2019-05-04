adefc86a15d773b546fd5ede7165f7607248fe17
jonaslopes19 <jonasl1@insper.al.edu.br>
2019-05-04 16:54:00 -0300

    História inicial do jogo

---
+++ históriadojogo.py
@@ -0,0 +1,12 @@
+Está é a história de Anélthei(Ascender) da Terra, 
+era assim conhecido na Terra e também em Neró Fengári(Lua d’água). 
+Lua de um planeta distante. Na Terra é conhecido por Tei, 
+o qual era o aluno mais popular da universidade, capitão do time de futsal e prodígio do curso de economia, 
+era também fã de Heyk e Friedman. Tei sofreu um acidente de carro após sair de uma festa porque o bateu. 
+Consequências foram um traumatismo craniano e uma lesão medular ou traumatismo raquemedular(T.R.M.).~
+Enquanto estava em como ascendeu em Neró Fengali, no corpo do filho do rei Ischyró Kai Dikaio(Poderoso e Justo). 
+O rei possuía três filhos, duas meninas e um menino, que era chamado de príncipe sem nome, 
+pois nascera em um sono eterno. O mago Sofós(Sábio) havia tido uma revelação que no primeiro 
+eclipse lunar depois das guerras de Érimo(deserto). Os Apó Makriá(Os que vieram de longe) voltariam e com um exécito 
+poderoso e com a ajuda de um feiticeiro(Iaimodipsís). Mas, o príncipe que ascende de outro mundo srugirá para salvar 
+o povo de Neró Fengári(Lua d’água)
