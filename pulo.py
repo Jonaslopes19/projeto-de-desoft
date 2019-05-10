a4917d87ff94294a9e9127f6e9f7c4c25dd123ee
jonaslopes19 <jonasl1@insper.al.edu.br>
2019-05-10 15:29:54 -0300

    jonas

---
+++ pulo.py
@@ -0,0 +1,13 @@
+import pygame
+from os import path
+from init import HEIGHT, WIDTH, BLACK, img_dir, snd_dir, fundos, FPS
+from player import Player
+
+
+def pulo(Player()):
+    self.speedy = -15
+    g = 5
+    while self.rect.y < HEIGHT/2 + 40:
+        self.speedy += g        
+    
+    
---
+++ sem título6.py
@@ -0,0 +1,88 @@
+import pygame
+from os import path
+from init import HEIGHT, WIDTH, BLACK, img_dir, snd_dir, fundos, FPS
+from player import Player
+from Bullet import Bullet
+from pulo import pulo
+
+pygame.init()
+pygame.mixer.init()
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Rumo a Nero")
+# Variável para o ajuste de velocidade
+clock = pygame.time.Clock()
+
+# Carrega o fundo do jogo
+background = pygame.image.load(path.join(fundos, 'Fundo.png')).convert()
+background_rect = background.get_rect()
+x = 0 
+vx=0
+
+# Carrega os sons do jogo
+pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.4)
+boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav'))
+destroy_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl6.wav'))
+pew_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav'))
+
+player = Player()
+
+# Cria um grupo de todos os sprites e adiciona a nave.
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+bullets = pygame.sprite.Group()
+
+try:
+    
+    pygame.mixer.music.play(loops=-1)
+    running = True
+    while running:
+        
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    vx = 8
+                if event.key == pygame.K_RIGHT:
+                    vx = -8
+                if event.key == pygame.K_UP:
+                    pulo(player)
+                if event.key == pygame.K_SPACE:
+                    bullet = Bullet(player.rect.centerx, player.rect.top)
+                    all_sprites.add(bullet)
+                    bullets.add(bullet)
+                    pew_sound.play()
+                    
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    vx = 0
+                if event.key == pygame.K_RIGHT:
+                    vx = 0
+                    
+        # Depois de processar os eventos.
+        # Atualiza a acao de cada sprite.
+        all_sprites.update()
+    
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        x+= vx
+        screen.blit(background, (x, 0))
+        all_sprites.draw(screen)
+        
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+        
+finally:
+    
+    pygame.quit()
