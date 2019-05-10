61809107d36f643ee6bbcba5b8f2734e9afe880f
jonaslopes19 <jonasl1@insper.al.edu.br>
2019-05-10 14:44:13 -0300

    Organizadão

---
+++ Bullet.py
@@ -0,0 +1,34 @@
+import pygame
+from os import path
+from init import HEIGHT, WIDTH, BLACK, img_dir, snd_dir, fundos, FPS
+
+class Bullet(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self, x, y):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
+        self.image = bullet_img
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.bottom = y + 70
+        self.rect.centerx = x + 40
+        self.speedx = 10
+
+    # Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect.x += self.speedx
+        
+        # Se o tiro passar do inicio da tela, morre.
+        if self.rect.bottom < 0:
+            self.kill()
--- código_teste.py
+++ código_teste.py
@@ -1,107 +1,15 @@
-# -*- coding: utf-8 -*-
-
-# Importando as bibliotecas necessárias.
 import pygame
-import random
-#import time
 from os import path
 from init import HEIGHT, WIDTH, BLACK, img_dir, snd_dir, fundos, FPS
-#from fundo import fundo
 from player import Player
+from Bullet import Bullet
 
-
-
-def goup(self):
-    self.rect.bottom=HEIGHT/2 + 40
-def position(self):
-    y= self.rect.bottom
-    return y                 
-# Classe Mob que representa os meteoros
-class Mob(pygame.sprite.Sprite):
-    
-    # Construtor da classe.
-    def __init__(self):
-        
-        # Construtor da classe pai (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-        
-        # Carregando a imagem de fundo.
-        mob_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.transform.scale(mob_img, (50, 38))
-        
-        # Deixando transparente.
-        self.image.set_colorkey(BLACK)
-        
-        # Detalhes sobre o posicionamento.
-        self.rect = self.image.get_rect()
-        
-        # Sorteia um lugar inicial em x
-        self.rect.x = random.randrange(WIDTH - self.rect.width)
-        # Sorteia um lugar inicial em y
-        self.rect.y = random.randrange(-100, -40)
-        # Sorteia uma velocidade inicial
-        self.speedx = random.randrange(-3, 3)
-        self.speedy = random.randrange(2, 9)
-        
-        # Melhora a colisão estabelecendo um raio de um circulo
-        self.radius = int(self.rect.width * .85 / 2)
-        
-    # Metodo que atualiza a posição da navinha
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        
-        # Se o meteoro passar do final da tela, volta para cima
-        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-            self.rect.x = random.randrange(WIDTH - self.rect.width)
-            self.rect.y = random.randrange(-100, -40)
-            self.speedx = random.randrange(-3, 3)
-            self.speedy = random.randrange(2, 9)
-            
-# Classe Bullet que representa os tiros
-class Bullet(pygame.sprite.Sprite):
-    
-    # Construtor da classe.
-    def __init__(self, x, y):
-        
-        # Construtor da classe pai (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-        
-        # Carregando a imagem de fundo.
-        bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
-        self.image = bullet_img
-        
-        # Deixando transparente.
-        self.image.set_colorkey(BLACK)
-        
-        # Detalhes sobre o posicionamento.
-        self.rect = self.image.get_rect()
-        
-        # Coloca no lugar inicial definido em x, y do constutor
-        self.rect.bottom = y + 70
-        self.rect.centerx = x + 40
-        self.speedx = 10
-
-    # Metodo que atualiza a posição da navinha
-    def update(self):
-        self.rect.x += self.speedx
-        
-        # Se o tiro passar do inicio da tela, morre.
-        if self.rect.bottom < 0:
-            self.kill()
-
-# Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
 
-# Tamanho da tela.
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-# Nome do jogo
 pygame.display.set_caption("Rumo a Nero")
-
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
 
@@ -110,6 +18,7 @@ background = pygame.image.load(path.join(fundos, 'Fundo.png')).convert()
 background_rect = background.get_rect()
 x = 0 
 vx=0
+
 # Carrega os sons do jogo
 pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
 pygame.mixer.music.set_volume(0.4)
@@ -117,24 +26,16 @@ boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav'))
 destroy_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl6.wav'))
 pew_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav'))
 
-# Cria uma nave. O construtor será chamado automaticamente.
 player = Player()
 
 # Cria um grupo de todos os sprites e adiciona a nave.
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
-# Cria um grupo só dos meteoros
-
-
-# Cria um grupo para tiros
 bullets = pygame.sprite.Group()
 
-
-# Comando para evitar travamentos.
 try:
     
-    # Loop principal.
     pygame.mixer.music.play(loops=-1)
     running = True
     while running:
@@ -142,24 +43,20 @@ try:
         # Ajusta a velocidade do jogo.
         clock.tick(FPS)
         
-        # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
             
-            # Verifica se foi fechado.
             if event.type == pygame.QUIT:
                 running = False
             
-            # Verifica se apertou alguma tecla.
             if event.type == pygame.KEYDOWN:
-                # Dependendo da tecla, altera a velocidade.
                 if event.key == pygame.K_LEFT:
-                    vx = 50
+                    vx = 8
                 if event.key == pygame.K_RIGHT:
-                    vx = -50
+                    vx = -8
                 if event.key == pygame.K_UP:
                     player.speedy = -10
-
-                # Se for um espaço atira!
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 10
                 if event.key == pygame.K_SPACE:
                     bullet = Bullet(player.rect.centerx, player.rect.top)
                     all_sprites.add(bullet)
@@ -174,11 +71,12 @@ try:
                     player.speedx = 0
                 if event.key == pygame.K_RIGHT:
                     vx = 0
-                    player  .speedx = 0
+                    player.speedx = 0
                 if event.key == pygame.K_UP:
-                    position(player)
-                    goup(player)
                     player.speedy = 0
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 0
+                
                     
         # Depois de processar os eventos.
         # Atualiza a acao de cada sprite.
