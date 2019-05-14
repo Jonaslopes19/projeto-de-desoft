96423b882edbf7ef6bd91a00f9cd09b2a63ad28c
LaisNSilva <laisns@al.insper.edu.br>
2019-05-14 16:33:31 -0300

    (1)

---
+++ teste_lais.py
@@ -0,0 +1,126 @@
+import time
+import random
+import pygame
+from os import path
+from init import HEIGHT, WIDTH, BLACK, img_dir, snd_dir, fundos, FPS
+from player import Player
+from Bullet import Bullet
+from mob import Mob
+
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Rumo a Nero")
+
+clock = pygame.time.Clock()
+
+# Carrega o fundo do jogo
+background = pygame.image.load(path.join(fundos, 'Fundo.png')).convert()
+background_rect = background.get_rect()
+x = 0 
+vx=0
+
+# Carrega os sons do jogo
+pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.set_volume(0.4)
+boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav'))
+destroy_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl6.wav'))
+pew_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav'))
+
+player = Player()
+
+
+monsters = pygame.sprite.Group()
+
+# Cria um grupo de todos os sprites e adiciona a nave.
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+bullets = pygame.sprite.Group()
+
+try:
+    
+    pygame.mixer.music.play(loops=-1)
+    running = True
+    while running:
+        
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+
+        if random.randrange(1,100) == 1:
+            mob = Mob()
+            # Cria um grupo só do inimigo
+            all_sprites.add(mob)
+            monsters.add(mob)
+        
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    vx = 8
+                if event.key == pygame.K_RIGHT:
+                    vx = -8
+                if event.key == pygame.K_UP:
+                    player.speedy = -10
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 10
+                if event.key == pygame.K_SPACE:
+                    bullet = Bullet(player.rect.centerx, player.rect.top)
+                    all_sprites.add(bullet)
+                    bullets.add(bullet)
+                    pew_sound.play()
+                    
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    vx = 0
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    vx = 0
+                    player.speedx = 0
+                if event.key == pygame.K_UP:
+                    player.speedy = 0
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 0
+        
+        # Verifica se houve colisão entre tiro e inimigo
+        hits = pygame.sprite.groupcollide(monsters, bullets, True, True)
+        for hit in hits: # Pode haver mais de um
+            # O meteoro e destruido e precisa ser recriado
+            destroy_sound.play()
+            #m = Mob() 
+            #all_sprites.add(m)
+            #monsters.add(m)
+        
+        # Verifica se houve colisão entre personagem e inimigo
+        hits = pygame.sprite.spritecollide(player, monsters, False, pygame.sprite.collide_circle)
+        if hits:
+            # Toca o som da colisão
+            boom_sound.play()
+            time.sleep(1) # Precisa esperar senão fecha
+            
+            running = False
+                    
+        # Depois de processar os eventos.
+        # Atualiza a acao de cada sprite.
+        all_sprites.update()
+    
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        x+= vx
+        screen.blit(background, (x, 0))
+        all_sprites.draw(screen)
+        
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+        
+        
+        
+finally:
+    
+    pygame.quit()
