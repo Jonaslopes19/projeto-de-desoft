4aa194a49651d12caf1657869258e13c4971218b
Eiki Yamashiro Santos <49211526+EikiSantos@users.noreply.github.com>
2019-05-14 17:13:21 -0300

    Mob sasuke

--- mob.py
+++ mob.py
@@ -13,7 +13,7 @@ class Mob(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         # Carregando a imagem de fundo.
-        mob_img = pygame.image.load(path.join(mobs, "Mumia.png")).convert()
+        mob_img = pygame.image.load(path.join(mobs, "mob1.png")).convert()
         
         # Diminuindo o tamanho da imagem.
         self.image = pygame.transform.scale(mob_img, (50, 70))
