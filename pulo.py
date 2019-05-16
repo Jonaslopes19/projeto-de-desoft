6a9cf7cfdb49936589d9a2da60a6a44f5b8f8cdb
jonaslopes19 <jonasl1@insper.al.edu.br>
2019-05-16 10:10:59 -0300

    Show

--- pulo.py
+++ pulo.py
@@ -5,20 +5,5 @@ from player import Player
 
 
 def Pulo(player):
-    player.speedy = -15
-    g = 5
-    while player.rect.y < HEIGHT/2 + 40:
-        player.speedy += g        
+    v0 = -10
     
-        # Depois de processar os eventos.
-        # Atualiza a acao de cada sprite.
-        all_sprites.update()
-    
-        # A cada loop, redesenha o fundo e os sprites
-        screen.fill(BLACK)
-        x+= vx
-        screen.blit(background, (x, 0))
-        all_sprites.draw(screen)
-        
-        # Depois de desenhar tudo, inverte o display.
-        pygame.display.flip()
