f90ca4aafd2cbd63fc2c7096afd22d844c4a514d
Eiki Yamashiro Santos <49211526+EikiSantos@users.noreply.github.com>
2019-05-17 09:55:44 -0300

    Imagem de fundo adicionada

--- area_teste_eiki.py
+++ area_teste_eiki.py
@@ -1,250 +1,247 @@
-# -*- coding: utf-8 -*-
-
-# Importando as bibliotecas necessárias.
-import pygame
-import random
 import time
+import random
+import pygame
 from os import path
+from init import HEIGHT, WIDTH, BLACK, img_dir, snd_dir, fundos, FPS
+from player import Player
+from Bullet import Bullet
+from mob import Mob
+from Mob2 import Mob2
+from Mob3 import Mob3
+from Rasengan import Rasengan
 
-# Estabelece a pasta que contem as figuras e sons.
-img_dir = path.join(path.dirname(__file__), 'img')
-snd_dir = path.join(path.dirname(__file__), 'snd')
-fundos = path.join(path.dirname(__file__), 'Fundos')
-personagem = path.join(path.dirname(__file__), 'Personagem1')
-
-
-# Dados gerais do jogo.
-WIDTH = 1000 # Largura da tela
-HEIGHT = 445 # Altura da tela
-FPS = 30 # Frames por segundo
-
-# Define algumas variáveis com as cores básicas
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-
-# Classe Jogador que representa a nave
-class Player(pygame.sprite.Sprite):
-    
-    # Construtor da classe.
-    def __init__(self):
-        
-        # Construtor da classe pai (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-        
-        # Carregando a imagem de fundo.
-        player_img = pygame.image.load(path.join(personagem, "p1.png")).convert()
-        self.image = player_img
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.transform.scale(player_img, (75, 100))
-        
-        # Deixando transparente.
-        self.image.set_colorkey(BLACK)
-        
-        # Detalhes sobre o posicionamento.
-        self.rect = self.image.get_rect()
-        
-        # Centraliza embaixo da tela.
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT/2 + 40
-        
-        # Velocidade da nave
-        self.speedx = 0
-        self.speedy = 0
-        
-        # Melhora a colisão estabelecendo um raio de um circulo
-        self.radius = 25
-    
-    # Metodo que atualiza a posição da navinha
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        # Mantem dentro da tela
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-def goup(self):
-    self.rect.bottom=HEIGHT/2 + 40
-def position(self):
-    y= self.rect.bottom
-    return y                 
-# Classe Mob que representa os meteoros
-class Mob(pygame.sprite.Sprite):
-    
-    # Construtor da classe.
-    def __init__(self):
-        
-        # Construtor da classe pai (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-        
-        # Carregando a imagem de fundo.
-        mob_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.transform.scale(mob_img, (50, 38))
-        
-        # Deixando transparente.
-        self.image.set_colorkey(BLACK)
-        
-        # Detalhes sobre o posicionamento.
-        self.rect = self.image.get_rect()
-        
-        # Sorteia um lugar inicial em x
-        self.rect.x = random.randrange(WIDTH - self.rect.width)
-        # Sorteia um lugar inicial em y
-        self.rect.y = random.randrange(-100, -40)
-        # Sorteia uma velocidade inicial
-        self.speedx = random.randrange(-3, 3)
-        self.speedy = random.randrange(2, 9)
-        
-        # Melhora a colisão estabelecendo um raio de um circulo
-        self.radius = int(self.rect.width * .85 / 2)
-        
-    # Metodo que atualiza a posição da navinha
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        
-        # Se o meteoro passar do final da tela, volta para cima
-        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-            self.rect.x = random.randrange(WIDTH - self.rect.width)
-            self.rect.y = random.randrange(-100, -40)
-            self.speedx = random.randrange(-3, 3)
-            self.speedy = random.randrange(2, 9)
-            
-# Classe Bullet que representa os tiros
-class Bullet(pygame.sprite.Sprite):
-    
-    # Construtor da classe.
-    def __init__(self, x, y):
-        
-        # Construtor da classe pai (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-        
-        # Carregando a imagem de fundo.
-        bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
-        self.image = bullet_img
-        
-        # Deixando transparente.
-        self.image.set_colorkey(BLACK)
-        
-        # Detalhes sobre o posicionamento.
-        self.rect = self.image.get_rect()
-        
-        # Coloca no lugar inicial definido em x, y do constutor
-        self.rect.bottom = y + 70
-        self.rect.centerx = x + 40
-        self.speedx = 10
-
-    # Metodo que atualiza a posição da navinha
-    def update(self):
-        self.rect.x += self.speedx
-        
-        # Se o tiro passar do inicio da tela, morre.
-        if self.rect.bottom < 0:
-            self.kill()
-
-# Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
 
-# Tamanho da tela.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Corrida Naruto")
 
-# Nome do jogo
-pygame.display.set_caption("Rumo a Nero")
-
-# Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
 
 # Carrega o fundo do jogo
 background = pygame.image.load(path.join(fundos, 'Fundo.png')).convert()
 background_rect = background.get_rect()
 x = 0 
+vx=0
+
 # Carrega os sons do jogo
-pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+pygame.mixer.music.load(path.join(snd_dir, 'naruto.mp3'))
 pygame.mixer.music.set_volume(0.4)
 boom_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl3.wav'))
 destroy_sound = pygame.mixer.Sound(path.join(snd_dir, 'expl6.wav'))
 pew_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav'))
 
-# Cria uma nave. O construtor será chamado automaticamente.
 player = Player()
+
+i=0
+# Cria um grupo só do inimigo
+monsters = pygame.sprite.Group()
 
 # Cria um grupo de todos os sprites e adiciona a nave.
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
-# Cria um grupo só dos meteoros
-
-
-# Cria um grupo para tiros
 bullets = pygame.sprite.Group()
 
-
-# Comando para evitar travamentos.
 try:
     
-    # Loop principal.
     pygame.mixer.music.play(loops=-1)
     running = True
     while running:
         
         # Ajusta a velocidade do jogo.
         clock.tick(FPS)
+        if random.randrange(1,100) == 1:
+            mob2 = Mob2()
+            all_sprites.add(mob2)
+            monsters.add(mob2)
+        if random.randrange(1,200) == 0:
+            mob3 = Mob3()
+            all_sprites.add(mob3)
+            monsters.add(mob3)
+        if random.randrange(1,200) == 0:
+            mob = Mob()
+            # Cria um grupo só do inimigo
+            all_sprites.add(mob)
+            monsters.add(mob)
         
-        # Processa os eventos (mouse, teclado, botão, etc).
+        
         for event in pygame.event.get():
             
-            # Verifica se foi fechado.
             if event.type == pygame.QUIT:
                 running = False
             
-            # Verifica se apertou alguma tecla.
             if event.type == pygame.KEYDOWN:
-                # Dependendo da tecla, altera a velocidade.
                 if event.key == pygame.K_LEFT:
-                    player.speedx = -8
+                    vx = 8
+                    player.imgs = []
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Left{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.step = 5
+                    player.image = player.imgs[player.frame]
+                    player.image = pygame.transform.scale(player.image, (1,1))
                 if event.key == pygame.K_RIGHT:
-                    player.speedx = 8
+                    vx = -8
+                    player.imgs = []
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Run{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.step = 5
+                    player.image = player.imgs[player.frame]
+                    player.image = pygame.transform.scale(player.image, (1,1))
                 if event.key == pygame.K_UP:
+                    player.image = pygame.image.load(path.join("JumpNar.png")).convert()
+                    player.image = pygame.transform.scale(player.image, (1,1))
+                    g = 1
+                    a = True
+                    
                     player.speedy = -10
-
-                # Se for um espaço atira!
+                    while a:
+                        if i == 0:
+                            player.speedy = -10
+                        elif player.rect.y < 177.5:
+                            player.rect.y += player.speedy
+                            player.speedy += g
+                            all_sprites.update()
+                            print(player.rect.y)
+                            time.sleep(1.7e-2)
+                            i=1
+                        else:
+                            a = False 
+                            i=1
+    
+                        # A cada loop, redesenha o fundo e os sprites
+                        screen.fill(BLACK)
+                        screen.blit(background, (x, 0))
+                        all_sprites.draw(screen)
+                        
+                        # Depois de desenhar tudo, inverte o display.
+                        pygame.display.flip()
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 10
                 if event.key == pygame.K_SPACE:
+                    player.imgs = []
+                    n=5
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Punch{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.image = player.imgs[player.frame]
+                    player.step=5
+                    player.image = pygame.transform.scale(player.image, (1,1))
                     bullet = Bullet(player.rect.centerx, player.rect.top)
                     all_sprites.add(bullet)
                     bullets.add(bullet)
                     pew_sound.play()
+                if event.key == pygame.K_m:
+                    player.imgs = []
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("R{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.image = player.imgs[player.frame]
+                    player.step=5
+                    player.image = pygame.transform.scale(player.image, (1,1))
+                    ras = Rasengan(player.rect.centerx, player.rect.top)
+                    all_sprites.add(ras)
+                    bullets.add(ras)
+                    pew_sound.play()
+
+                                
                     
             # Verifica se soltou alguma tecla.
             if event.type == pygame.KEYUP:
                 # Dependendo da tecla, altera a velocidade.
                 if event.key == pygame.K_LEFT:
+                    vx = 0
+                    player.imgs = []
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Naruto{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.image = player.imgs[player.frame]
+                    player.image = pygame.transform.scale(player.image, (1,1))
                     player.speedx = 0
                 if event.key == pygame.K_RIGHT:
+                    vx = 0
+                    player.imgs = []
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Naruto{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.image = player.imgs[player.frame]
+                    player.image = pygame.transform.scale(player.image, (1,1))
                     player.speedx = 0
                 if event.key == pygame.K_UP:
-                    position(player)
-                    goup(player)
+                    player.imgs = []
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Naruto{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.image = player.imgs[player.frame]
+                    player.image = pygame.transform.scale(player.image, (1,1))
                     player.speedy = 0
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 0
+                if event.key == pygame.K_SPACE:
+                    player.imgs = []
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Run{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.image = player.imgs[player.frame]
+                    player.image = pygame.transform.scale(player.image, (1,1))
+                    player.speedx = 0
+                    player.step=5
+                if event.key == pygame.K_m:
+                    player.imgs = []
+                    player.steps = 5
+                    n=6
+                    for i in range(n):
+                        player.imgs.append(pygame.image.load(path.join("Run{0}.png".format(i+1))).convert())
+                    player.frame = 0
+                    player.steps = 5
+                    player.image = player.imgs[player.frame]
                     
+                    player.image = pygame.transform.scale(player.image, (1,1))
+                    player.speedx = 0
+
+        
+        # Verifica se houve colisão entre tiro e inimigo
+        hits = pygame.sprite.groupcollide(monsters, bullets, True, True)
+        for hit in hits: # Pode haver mais de um
+            # O meteoro e destruido e precisa ser recriado
+            destroy_sound.play()
+            #m = Mob() 
+            #all_sprites.add(m)
+            #monsters.add(m)
+        
+        # Verifica se houve colisão entre personagem e inimigo
+        hits = pygame.sprite.spritecollide(player, monsters, False, pygame.sprite.collide_circle)
+        if hits:
+            # Toca o som da colisão
+            boom_sound.play()
+            time.sleep(5) # Precisa esperar senão fecha
+            
+            running = False
+                    
+        
         # Depois de processar os eventos.
         # Atualiza a acao de cada sprite.
         all_sprites.update()
     
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
+        x+= vx
         screen.blit(background, (x, 0))
         all_sprites.draw(screen)
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
+        
+        
         
 finally:
     
