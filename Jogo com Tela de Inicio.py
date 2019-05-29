a2a973b23d230717d9b3266798b940de18a7a79c
jonaslopes19 <jonasl1@insper.al.edu.br>
2019-05-29 00:07:30 -0300

    jonas

--- Jogo com Tela de Inicio.py
+++ Jogo com Tela de Inicio.py
@@ -12,7 +12,8 @@ pygame.init()
 pygame.mixer.init()
 
 # Tamanho da tela.
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WI
+                                  DTH, HEIGHT))
 
 # Nome do jogo
 pygame.display.set_caption("Naruto Run")
